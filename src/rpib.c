0ff735d1b2a35e59f4bbe88c147a4ff876449dfc
Thijskamphuis <thijskamphuis@icloud.com>
2025-02-13 13:49:20 +0100

    sleep

--- src/rpib.c
+++ src/rpib.c
@@ -1,12 +1,13 @@
 #include <../include/socket.h>
-#include <stdio.h>  
+#include <stdio.h>
+#include <unistd.h>
 
 #define IP "145.52.127.118"
 #define PORT 8080
 
 int main() {
     connectToSocket(IP, PORT);
-    delay(100);
+    sleep(1);
     sendData("Hello world");
     closeSocket();
 
