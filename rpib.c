c83483156023d2119a5a7101683119860c35890a
Thijskamphuis <thijskamphuis@icloud.com>
2025-02-13 12:54:03 +0100

    init test

---
+++ rpib.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "rpia.local", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Send message to the server
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // Read response from the server
+    int valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+
+    // Close the socket
+    close(sock);
+
+    return 0;
+}
