430bee88c03bfb0902a7c83c6f411c8c189d5303
Thijskamphuis <thijskamphuis@icloud.com>
2025-03-20 15:42:39 +0100

    rpoa

--- src/rpia.c
+++ src/rpia.c
@@ -1,19 +1,46 @@
 #include <../include/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
 
-#define PORT 8380
+#define I2C_BUS "/dev/i2c-1"
+#define SLAVE_ADDR 0x69 // MC-BALIE
+
+#define PORT 8080
+
+int setupI2C(const char *bus, int slave_addr) {
+    int file = open(bus, O_RDWR);
+    ioctl(file, I2C_SLAVE, slave_addr);
+    return file;
+}
+
 
 int main() {
     hostSocket(PORT);
+
+    int file;
+    char *bus = I2C_BUS;
+    unsigned char buffer[60];
+    file = setupI2C(bus, SLAVE_ADDR);
     while (1)
     {
         acceptConnection();
-        char *data = listenForData();
-        printf("%s\n", data);
-        // if (data != NULL)
-        // {
-        //     printf("Data received: %s\n", data);
-        // }
+        if (read(file, buffer, sizeof(buffer)) > 0) {
+            printf("Data read: %s\n", buffer);
+            if (buffer[0] == '1') {
+                sendData("1")
+                write(file, buffer, 1);
+            } else if (buffer[0] == '0') {
+                sendData("0")
+            }
+        }
+
+
+        
     }
     
     return 0;
--- src/socket.c
+++ src/socket.c
@@ -3,15 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <../include/socket.h>
 
 #define BACKLOG 10
 
 int server_fd, new_socket;
-struct sockaddr_in address;
-int addrlen = sizeof(address);
-char ip_str[INET_ADDRSTRLEN];
-
 
 /*!
  * \brief Create socket
@@ -19,35 +16,53 @@ char ip_str[INET_ADDRSTRLEN];
  * \param port De poort waarop de socket moet worden aangemaakt
  */
 void hostSocket(int port) {
+    struct sockaddr_in address;
     int opt = 1;
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    int addrlen = sizeof(address);
 
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
+    if (listen(server_fd, BACKLOG) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    listen(server_fd, BACKLOG);
+    printf("Server is listening on port %d\n", port);
 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    int flags = fcntl(new_socket, F_GETFL, 0);
+    fcntl(new_socket, F_SETFL, flags | O_NONBLOCK);
+    printf("Connection accepted\n");
 
-    printf("Server is listening on port %d\n", port);  
+    
 }
-
-/*!
- * \brief Accept incoming connection
- * \details Accepteert een inkomende verbinding op de socket
- */
-void acceptConnection() {
-    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    inet_ntop(AF_INET, &address.sin_addr, ip_str, sizeof(ip_str));
-
-    printf("Connection accepted, client IP: %s\n", ip_str);
-}
-
 /*!
  * \brief Close socket
  * \details Sluit de socket
@@ -65,7 +80,7 @@ void closeSocket() {
  * \param port De poort waarmee verbonden moet worden
  */
 
-void connectToSocket(const char *ip, int port) {
+void connectToSocket(const char *host, int port) {
     struct sockaddr_in serv_addr;
 
     if ((new_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -78,7 +93,7 @@ void connectToSocket(const char *ip, int port) {
 
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, gethostbyname(host), &serv_addr.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
@@ -88,7 +103,7 @@ void connectToSocket(const char *ip, int port) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to %s:%d\n", ip, port);
+    printf("Connected to (%s) %s:%d\n", host, gethostbyname(host), port);
 }
 
 /*!
@@ -110,15 +125,15 @@ char *listenForData() {
     static char buffer[1024] = {0};
     int valread;
 
-    return read(new_socket, buffer, sizeof(buffer) - 1);
+    valread = read(new_socket, buffer, sizeof(buffer) - 1);
+    if (valread > 0) {
+        buffer[valread] = '\0';
+        return buffer;
+    }
 
-    // valread = read(new_socket, buffer, sizeof(buffer) - 1);
-    // if (valread > 0) {
-    //     buffer[valread] = '\0';
-    //     return buffer;
-    // }
-    // if (valread == 0) {
-    //     printf("Client disconnected\n");
-    // }
-    //return NULL;
+    if (valread < 0) {
+        perror("read");
+    }
+
+    return NULL;
 }
